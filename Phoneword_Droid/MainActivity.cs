7a69b22759794ce4966e02346536e02ab30d4c6b
Fengming Sun <s@sfmblog.cn>
2015-01-12 02:09:19 +0800

    first commit

---
+++ Phoneword_Droid/MainActivity.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Phoneword_Droid
+{
+	[Activity(Label = "Phoneword", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		static readonly List<String> phoneNumbers = new List<String>();
+
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+			Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+			Button callButton = FindViewById<Button>(Resource.Id.CallButton);
+			Button callHistoryButton = FindViewById<Button>(Resource.Id.CallHistoryButton);
+
+			callButton.Enabled = false;
+
+			string translatedNumber = string.Empty;
+
+			translateButton.Click += (object sender, EventArgs e) => {
+				translatedNumber = Phoneword.PhoneTranslator.ToNumber(phoneNumberText.Text);
+
+				if (String.IsNullOrWhiteSpace(translatedNumber)) {
+					callButton.Text = "Call";
+					callButton.Enabled = false;
+				} else {
+					callButton.Text = "Call " + translatedNumber;
+					callButton.Enabled = true;
+				}
+			};
+
+			callButton.Click += (object sender, EventArgs e) => {
+				phoneNumbers.Add(translatedNumber);
+
+				var callDialog = new AlertDialog.Builder(this);
+				callDialog.SetTitle ("Call");
+				callDialog.SetMessage("Call " + translatedNumber + "?");
+				callDialog.SetNeutralButton("Call", delegate {
+					var callIntent = new Intent(Intent.ActionCall);
+					callIntent.SetData(Android.Net.Uri.Parse("tel:" + translatedNumber));
+					StartActivity(callIntent);
+				});
+
+				callDialog.SetNegativeButton("Cancel", delegate {
+				});
+
+				callDialog.Show();
+			};
+
+			callHistoryButton.Click += (object sender, EventArgs e) => {
+				var intent = new Intent (this, typeof(CallHistoryActivity));
+				intent.PutStringArrayListExtra ("phone_numbers", phoneNumbers);
+				StartActivity (intent);
+			};
+		}
+	}
+}
+
+
---
+++ Phoneword_iOS/Phoneword_iOSViewController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Drawing;
+
+using Foundation;
+using UIKit;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Phoneword_iOS
+{
+	public partial class Phoneword_iOSViewController : UIViewController
+	{
+
+		public List<String> PhoneNumbers { get; set; }
+
+		public Phoneword_iOSViewController(IntPtr handle)
+			: base(handle)
+		{
+			PhoneNumbers = new List<String>();
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		#region View lifecycle
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			
+			// Perform any additional setup after loading the view, typically from a nib.
+			string translatedNumber = "";
+
+			TranslateButton.TouchUpInside += (object sender, EventArgs e) => {
+				translatedNumber = Phoneword.PhoneTranslator.ToNumber(PhoneNumberText.Text);
+				PhoneNumberText.ResignFirstResponder();
+
+				if (translatedNumber == "") {
+					CallButton.SetTitle("Call", UIControlState.Normal);
+					CallButton.Enabled = false;
+				} else {
+					CallButton.SetTitle("Call " + translatedNumber, UIControlState.Normal);
+					CallButton.Enabled = true;
+				}
+			};
+
+			CallButton.TouchUpInside += (object sender, EventArgs e) => {
+
+				PhoneNumbers.Add(translatedNumber);
+
+				var url = new NSUrl("tel:" + translatedNumber);
+
+
+				var confirmDialog = new UIAlertView("Call", "Call " + translatedNumber + "?", null, "Cancel", new string[] {"Call"});
+
+				confirmDialog.Clicked += (object sd, UIButtonEventArgs es) => {
+					if (es.ButtonIndex == 1) {
+						if (!UIApplication.SharedApplication.OpenUrl(url)) {
+							var av = new UIAlertView("Not supported",
+								        "Scheme 'tel:' is not supported on this device", null, "OK", null);
+							av.Show();
+						}
+					}
+				};
+
+				confirmDialog.Show();
+
+			};
+
+			CallHistoryButton.TouchUpInside += (object sender, EventArgs e) => {
+				CallHistoryController callHistory = this.Storyboard.InstantiateViewController("CallHistoryController") as CallHistoryController;
+				if (callHistory != null) {
+					callHistory.PhoneNumbers = PhoneNumbers;
+				}
+				this.NavigationController.PushViewController(callHistory, true);
+			};
+		}
+
+		//		public override void PrepareForSegue (UIStoryboardSegue segue, NSObject sender)
+		//		{
+		//			base.PrepareForSegue (segue, sender);
+		//
+		//			var callHistoryController = segue.DestinationViewController as CallHistoryController;
+		//
+		//			if (callHistoryController != null) {
+		//				callHistoryController.PhoneNumbers = PhoneNumbers;
+		//			}
+		//		}
+
+		public override void ViewWillAppear(bool animated)
+		{
+			base.ViewWillAppear(animated);
+		}
+
+		public override void ViewDidAppear(bool animated)
+		{
+			base.ViewDidAppear(animated);
+		}
+
+		public override void ViewWillDisappear(bool animated)
+		{
+			base.ViewWillDisappear(animated);
+		}
+
+		public override void ViewDidDisappear(bool animated)
+		{
+			base.ViewDidDisappear(animated);
+		}
+
+		#endregion
+	}
+}
+
